92158e8ec07daff124088a607c98c20b9441995f
Marcelo-Diament <marcelodiament@gmail.com>
2021-06-13 16:34:04 -0300

    feature: update main script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,29 @@
+import { selector, setAttributes } from './helpers/general.js'
 import makeRequest from './api/makeRequest.js'
-import { displayUserDetails } from './inc/user.js'
+import { displayUserDetails, displayUserRepos } from './inc/user.js'
 
 const loadAndShowUserDetails = name => makeRequest(name, 'user', displayUserDetails)
+const loadAndShowUserRepos = name => makeRequest(name, 'repos', displayUserRepos)
+
+const addBtnSeeMore = name => {
+  const userArticle = selector('.user article')[0],
+    btnSeeMore = document.createElement('btn'),
+    btnSeeMoreTxt = document.createTextNode('Ver Repositórios'),
+    loadUserRepos = () => loadAndShowUserRepos(name)
+  setAttributes(btnSeeMore, {
+    class: 'btn btn--see-more',
+    'data-name': name,
+    id: 'seeRepos',
+    href: '#'
+  })
+  btnSeeMore.addEventListener('click', loadUserRepos)
+  btnSeeMore.appendChild(btnSeeMoreTxt)
+  userArticle.appendChild(btnSeeMore)
+}
 
 const loadContent = name => {
   loadAndShowUserDetails(name)
+  addBtnSeeMore(name)
 }
 
 window.onload = () => {
