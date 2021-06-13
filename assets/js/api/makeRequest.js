ad905aa604d14a3a28203139b9f2e751bab2c6fb
Marcelo-Diament <marcelodiament@gmail.com>
2021-06-13 16:32:58 -0300

    feature: update request

--- assets/js/api/makeRequest.js
+++ assets/js/api/makeRequest.js
@@ -1,7 +1,12 @@
-// Responsável por realizar a request AJAX via axios
-
 const makeRequest = (username, purpose, callback) => {
   let endpoint = `${username}`
+  switch (purpose) {
+    case 'repos':
+      endpoint += `/repos?type=public&sort=updated&direction=desc`
+      break
+    default:
+      endpoint
+  }
   axios.get(`https://api.github.com/users/${endpoint}`)
     .then(response => callback(response.data))
     .catch(error => console.error(error))
